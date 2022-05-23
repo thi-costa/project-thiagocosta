7efd77ec4c3e15ee8b8ed3e636c900936982fc38
thi-costa <thiago14abr@gmail.com>
2022-05-23 16:38:49 -0300

    App component updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Aluno, listaAlunos } from './arquivo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'project-thiagocosta';
+export class AppComponent { // Componentes do App
+  title = 'Lista de Alunos'; // Título da página do app
+  alunos: Aluno[] = listaAlunos; // Lista de alunos
 }
